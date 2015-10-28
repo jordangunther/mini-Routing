3b9ad8617b7507b8105563099194fd9680f2f2fa
jordangunther <jordankgunther@gmail.com>
2015-10-27 19:59:33 -0600

    mini-Routing complete and understood

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('miniRouting', ['ngRoute'//,
+  // 'controllers'
+]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'js/home/homeTmpl.html',
+    controller: 'homeCtrl'
+  })
+  .when('/products/:id', {
+    templateUrl: 'js/products/productTmpl.html',
+    controller: 'productsCtrl'
+  })
+  .when('/settings', {
+    templateUrl: 'js/settings/settingsTmpl.html',
+    controller: 'settingsCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
+});
